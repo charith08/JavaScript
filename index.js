646abc9b5578bdbf948266fe1647a5fef688d72b
charith08 <42198220+charith08@users.noreply.github.com>
2018-12-04 20:01:08 +0530

    unique array of elements is created and iterated

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-function generateJson() {
+var count = 0;
+var max = 998;
+var uniqueRandoms = [];
 
-  var randno = Math.floor(Math.random() * 998);
+function generaterand() {
+    
+    if (!uniqueRandoms.length) {
+        for (var i = 1; i <= max; i++) {
+            uniqueRandoms.push(i);
+        }
+    }
+    var index = Math.floor(Math.random() * uniqueRandoms.length);
+    var val = uniqueRandoms[index];
 
-  const url1 = "https://xkcd.now.sh/";
-  var ran = randno;
+    uniqueRandoms.splice(index, 1);
+    return val;
+}
 
-  const endpoint = `${url1}${ran}`;
 
-  var xhr = new XMLHttpRequest();
+for(var i = 1; i <= max; i++) {
+    function generateJson() {
+           const url1 = "https://xkcd.now.sh/";
+           var rand = generaterand();
+           const endpoint = `${url1}${rand}`;
 
-xhr.onreadystatechange = function()  {
-  if(this.readyState == 4 && this.status == 200)
-     {
-       var jcontent = JSON.parse(this.responseText);
-       document.getElementById('comicdisplay').src = jcontent.img;
-     }
-  };
+           var xhr = new XMLHttpRequest();
 
-xhr.open('GET', endpoint,true);
-xhr.send();
+           xhr.onreadystatechange = function()  {
+              if(this.readyState == 4 && this.status == 200)
+                {
+                   var jcontent = JSON.parse(this.responseText);
+                   document.getElementById('comicdisplay').src = jcontent.img;
+                }
+           };
 
+    xhr.open('GET', endpoint,true);
+    xhr.send();
+
+    count=count+1;
+    if(count == max+1 ) {
+		    alert("you've read all the comics");
+    }
+ }
 }
