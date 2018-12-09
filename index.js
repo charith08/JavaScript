9ddc7161ce4dfbece06c29a542ed0887062cb936
charith08 <42198220+charith08@users.noreply.github.com>
2018-12-09 12:32:05 +0530

    Version 2

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
-var count = 0;
-var max = 998;
-var uniqueRandoms = [];
-
-function generaterand() {
-    
-    if (!uniqueRandoms.length) {
-        for (var i = 1; i <= max; i++) {
-            uniqueRandoms.push(i);
-        }
-    }
-    var index = Math.floor(Math.random() * uniqueRandoms.length);
-    var val = uniqueRandoms[index];
-
-    uniqueRandoms.splice(index, 1);
-    return val;
+function generateJson()
+{
+if (localStorage.getItem("arr")===null)
+{
+  var arr = new Array();
+    localStorage.setItem("arr",JSON.stringify(arr));
+    console.log("hjsek");
 }
+  var arr= JSON.parse(localStorage.getItem("arr"));
+  if(arr.length==999)
+  {
+    alert("You've read all the comics");
+  }
+  else
+  {
+  console.log(arr);
+  var x=0;
+
+    while(x==0)
+    {
+      var num= Math.floor(Math.random()*999)+1;
+      console.log(num);
+      var flag=0;
+      arr.forEach(function(elem)
+      {
+        if (num== elem)
+          flag =1;
+      });
+      if(flag==0)
+      {
+        x=1;
+        var x= JSON.stringify(num);
+        arr.push(x);
+        localStorage.setItem("arr",JSON.stringify(arr));
+        var p= localStorage.getItem("arr");
+        console.log(p);
+      }
+    }
 
 
-for(var i = 1; i <= max; i++) {
-    function generateJson() {
+
            const url1 = "https://xkcd.now.sh/";
-           var rand = generaterand();
-           const endpoint = `${url1}${rand}`;
+           var rand = num;
+
+           var endpoint = `${url1}${rand}`;
 
            var xhr = new XMLHttpRequest();
 
@@ -35,10 +56,5 @@ for(var i = 1; i <= max; i++) {
 
     xhr.open('GET', endpoint,true);
     xhr.send();
-
-    count=count+1;
-    if(count == max+1 ) {
-		    alert("you've read all the comics");
-    }
  }
 }
